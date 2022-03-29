daa6aad713be04236a172dd47875aa7117f51648
timglabisch <glabisch@easybill.de>
2022-03-29 10:00:08 +0200

    kindA works

--- mehsh_check/src/analysis/analysis_command.rs
+++ mehsh_check/src/analysis/analysis_command.rs
@@ -1,102 +1,167 @@
 use std::process::{ExitStatus, Output, Stdio};
 use anyhow::{anyhow, Context};
+use chrono::{DateTime, Utc};
 use futures::channel;
-use tokio::io::{AsyncReadExt, BufReader};
+use tokio::fs::OpenOptions;
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::process::Command;
 use tokio::sync::broadcast::Receiver;
 use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
+use tokio::task::JoinHandle;
 use mehsh_common::config::ConfigAnalysis;
 use crate::BroadcastEvent;
 
 type CliOutput = Vec<u8>;
 
 pub struct ExecuteAnalysisCommandHandler {
-    config_analysis: ConfigAnalysis,
     notify_send: UnboundedSender<()>,
-    notify_recv: UnboundedReceiver<()>
+}
+
+struct CommandExecutionContext {
+    jh: JoinHandle<()>,
+    content: Vec<u8>,
+    started: DateTime<Utc>,
 }
 
 impl ExecuteAnalysisCommandHandler {
 
-    pub async fn new(config_analysis : ConfigAnalysis) -> Self {
+    pub fn new(config_analysis : ConfigAnalysis) -> Self {
         let (notify_send, notify_recv) = unbounded_channel::<()>();
 
-        Self {
-            config_analysis,
+        let s = Self {
             notify_send,
-            notify_recv
-        }
+        };
+
+        ::tokio::spawn(async move {
+            Self::execute(config_analysis, notify_recv).await;
+        });
+
+        s
     }
 
-    pub async fn run_if_not_running(&self) {
-        self.notify_send.send(());
+    pub fn run_if_not_running(&self) {
+        self.notify_send.send(()).expect("could not notify ExecuteAnalysisCommandHandler");
     }
 
-    async fn execute(&mut self, config : &ConfigAnalysis) {
-        let (sender, mut receiver) = ::tokio::sync::mpsc::unbounded_channel::<CliOutput>();
-        let (notify_command_finished, mut notify_receiver_finished) = unbounded_channel::<()>();
+    async fn execute(config_analysis : ConfigAnalysis, mut notify_recv : UnboundedReceiver<()>) {
+        let (execute_sender, mut execute_receiver) = ::tokio::sync::mpsc::unbounded_channel::<CliOutput>();
+        let (notify_command_finished_sender, mut notify_command_finished_receiver) = unbounded_channel::<()>();
 
-        let mut jh = None;
+        let mut command_execution_context = None;
 
         loop {
             ::tokio::select! {
                 // called when we need to run the command.
-                notify = self.notify_recv.recv() => {
+                notify = notify_recv.recv() => {
                     if notify.is_none() {
                         continue;
                     }
 
-                    if jh.is_some() {
-                        println!("command already running");
+                    if command_execution_context.is_some() {
+                        // already running
                         continue;
                     }
 
 
-                    let execute_config = config.clone();
-                    let execute_sender = sender.clone();
-                    let notify_command_finished = notify_command_finished.clone();
-                    jh = Some(::tokio::spawn(async move {
+                    let execute_config = config_analysis.clone();
+                    let execute_sender = execute_sender.clone();
+                    let notify_command_finished = notify_command_finished_sender.clone();
+                    let jh = ::tokio::spawn(async move {
                         execute_analysis_command(&execute_config, execute_sender).await;
                         notify_command_finished.send(()).expect("could not send notify_command_finished");
-                    }));
+                    });
 
-                    format!("started command.");
+                    command_execution_context = Some(CommandExecutionContext {
+                        jh,
+                        content: vec![],
+                        started: Utc::now(),
+                    });
+
+                    println!("started analysis {}.", &config_analysis.name);
                 },
                 // command finished
-                res = notify_receiver_finished.recv() => {
+                res = notify_command_finished_receiver.recv() => {
                     if res.is_none() {
                         continue;
                     }
 
-                    jh.as_mut().unwrap().abort(); // it must be already abortet.
-                    jh = None;
+                    let context = match command_execution_context {
+                        None => {
+                            println!("ERROR: command execution must exists");
+                            continue;
+                        },
+                        Some(ref mut c) => c,
+                    };
 
-                    println!("finished command");
+                    context.jh.abort(); // it must be already aborted.
 
-                    format!("finished command, exit");
+
+                    println!("finished analysis {}", &config_analysis.name);
+
+                    match write_report_file(&config_analysis, &context).await {
+                        Ok(filename) => {
+                            println!("wrote analysis {} report to {}", &config_analysis.name, filename);
+                        },
+                        Err(e) => {
+                            println!("Warning, could not write report {}", e);
+                        }
+                    };
+
+                    command_execution_context = None;
                 },
-                res = receiver.recv() => {
-                    let res = match res {
+                res = execute_receiver.recv() => {
+                    let mut res = match res {
                         Some(s) => s,
                         None => continue,
                     };
 
-                    println!("command output: {}", String::from_utf8_lossy(&res));
+                    println!("analysis {} output: {}", &config_analysis.name, String::from_utf8_lossy(&res));
+
+                    match command_execution_context {
+                        None => println!("warning, command execution context is empty. should never happen"),
+                        Some(ref mut context) => {
+                            context.content.append(&mut res)
+                        }
+                    };
                 }
             };
         }
-
-
-        // jh.abort();
-
     }
 }
 
+async fn write_report_file(config_analysis : &ConfigAnalysis, command_execution_context: &CommandExecutionContext) -> Result<String, ::anyhow::Error> {
 
-pub async fn execute_analysis_command(config : &ConfigAnalysis, sender: UnboundedSender<CliOutput>) -> Result<ExitStatus, ::anyhow::Error> {
+    let directory = format!("/tmp/mehsh/{}/{}", &config_analysis.name, &config_analysis.to.identifier);
+
+    ::tokio::fs::create_dir_all(&directory).await?;
+
+    let end_date = Utc::now();
+    let filename = format!("{}/{}.txt", &directory, end_date.format("%Y_%m_%d_%H_%M_%S"));
+
+    let mut file = OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .append(true)
+        .open(&filename)
+        .await
+        .context("could not create report file")?;
+
+    file.write_all(command_execution_context.content.as_slice()).await.context("write command output")?;
+
+    Ok(filename)
+}
+
+ fn get_command_with_variables(config : &ConfigAnalysis) -> String {
+     config.command.clone()
+         .replace("{{server.from.ip}}", &config.from.ip.to_string())
+         .replace("{{server.to.ip}}", &config.to.ip.to_string())
+         .to_string()
+}
+
+async fn execute_analysis_command(config : &ConfigAnalysis, sender: UnboundedSender<CliOutput>) -> Result<ExitStatus, ::anyhow::Error> {
     let mut command = Command::new("/bin/bash");
     let command_with_args = command
-        .args(&["-c", &config.command]);
+        .args(&["-c", &get_command_with_variables(config)]);
 
     let mut child = command_with_args
         .stdout(Stdio::piped())
@@ -124,10 +189,16 @@ pub async fn execute_analysis_command(config : &ConfigAnalysis, sender: Unbounde
         ::tokio::select! {
             stdout_read_res = stdout.read(&mut stdout_buffer) => {
                 let out : &[u8] = &stdout_buffer[..stdout_read_res.context("could not read stdout_read_res")?];
+                if out.len() == 0 {
+                    continue;
+                }
                 sender.send(out.to_vec())?;
             },
             stderr_read_res = stderr.read(&mut stderr_buffer) => {
                 let out : &[u8] = &stderr_buffer[..stderr_read_res.context("could not read stdout_read_res")?];
+                if out.len() == 0 {
+                    continue;
+                }
                 sender.send(out.to_vec())?;
             },
             res = child.wait() => {
--- mehsh_check/src/analyzer_event/analyzer_event_subscriber_analysis.rs
+++ mehsh_check/src/analyzer_event/analyzer_event_subscriber_analysis.rs
@@ -1,15 +1,17 @@
 use mehsh_common::config::ConfigAnalysis;
-use crate::BroadcastEvent;
+use crate::{BroadcastEvent, ExecuteAnalysisCommandHandler};
 use crate::udp_echo::analyzer_event::UdpEchoAnalyzerEventServer;
 
 pub struct AnalyzerEventSubscriberAnalysis {
     config_analysis: ConfigAnalysis,
-    broadcast_recv: ::tokio::sync::broadcast::Receiver<BroadcastEvent>
+    broadcast_recv: ::tokio::sync::broadcast::Receiver<BroadcastEvent>,
+    execute_analysis_command_handler: ExecuteAnalysisCommandHandler,
 }
 
 impl AnalyzerEventSubscriberAnalysis {
     pub fn new(config_analysis: ConfigAnalysis, broadcast_recv: ::tokio::sync::broadcast::Receiver<BroadcastEvent>) -> Self {
         Self {
+            execute_analysis_command_handler: ExecuteAnalysisCommandHandler::new(config_analysis.clone()),
             config_analysis,
             broadcast_recv
         }
@@ -30,6 +32,12 @@ impl AnalyzerEventSubscriberAnalysis {
     }
 
     pub async fn on_udp_echo_analyzer_event_server(&self, event: UdpEchoAnalyzerEventServer) {
+        let loss = event.req_count - event.resp_count;
 
+        if (loss as u32) < self.config_analysis.min_loss {
+            return;
+        }
+
+        self.execute_analysis_command_handler.run_if_not_running();
     }
 }
